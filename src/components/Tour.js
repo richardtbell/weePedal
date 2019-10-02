adac10e4557bff58f65c6b1e976932770541e96d
Olivia Dunnett <oliviadunnett@olivias-mbp.home>
2019-10-02 19:27:28 +0100

    https change in tours url

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -2,7 +2,7 @@ import React from "react";
 import { MDBCol, MDBCardBody, MDBBtn, MDBView, MDBMask } from "mdbreact";
 import ReactMarkdown from "react-markdown";
 const Tour = props => {
-  const imageUrl = "http://content.aweepedal.com" + props.image.url;
+  const imageUrl = "https://content.aweepedal.com" + props.image.url;
   return (
     <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
       <MDBView className="overlay rounded z-depth-1" waves>
