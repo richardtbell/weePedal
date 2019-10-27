9d67d39422377bfcf75ae777ca25f61db7ea8825
Richard Bell <richard.bell1@wipro.com>
2019-10-27 18:55:24 +0000

    Change order of tours

--- src/content/tours.js
+++ src/content/tours.js
@@ -1,6 +1,54 @@
 const tours =[
+  {
+    id: 1,
+    TourName: "Edinburgh to Portobello",
+    TourBlurb:
+      "Energy rating: Easy to Moderate. Families welcome if confident on a bike\r\n\r\nDistance: 12 miles\r\n\r\nTerrain: Short distances on roads. Traffic free cycle paths. All paved. \r\n\r\nWe shall start by cycling right beside Arthur's Seat, Edinburgh's extinct volcano and through Duddinston Village, passing Scotland's oldest surviving pub of 1860. We then cycle onto the National Cycle Network 1 path to Portobello beach. There we shall stop for a refreshment outside if sunny or indoors if the Scottish weather is against us! On our return journey we take\r\nthe Victorian-era Innocent Tunnel which was built in 1860 and goes underneath Holyrood Park. ",
+    created_at: "2019-05-19T14:47:30.000Z",
+    updated_at: "2019-09-11T14:19:54.000Z",
+    BookingLink:
+      "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10213399",
+    TourImage: {
+      id: 70,
+      name: "IMG_3034.jpeg",
+      hash: "39a24130bab04905bb9cef565337823d",
+      sha256: "LUKy_HnzZlyvvvIWvbj5d3Qpflod4RiItU8Qs2TD7vI",
+      ext: ".jpeg",
+      mime: "image/jpeg",
+      size: "2945.77",
+      url: "/uploads/39a24130bab04905bb9cef565337823d.jpeg",
+      provider: "local",
+      public_id: null,
+      created_at: "2019-09-11T14:19:54.000Z",
+      updated_at: "2019-09-11T14:19:54.000Z"
+    }
+  },
+  {
+    id: 2,
+    TourName: "The ultimate Edinburgh circular tour",
+    TourBlurb:
+      "Energy rating: Moderate.  Suitable for people who can ride for 3-4 hours with a stop in between and for families with children of 13+\r\n\r\nDistance: 18 miles\r\n\r\nTerrain:  Some short distances on roads with traffic lights. Traffic free cycle paths which are all paved. Gravel paths. We will be using a cycle ramp purpose built for bikes down a set of steps. \r\n\r\nWe will complete a full circle around Edinburgh city. We start and finish in The Meadows and I will show you the beautiful parts of Edinburgh most visitors do not get the chance to see. Canals, aqueducts, railways, gardens, beaches, mountains and tunnels. This tour is approx 18 miles and we will stop for refreshments on the way. It is mainly flat but Edinburgh is built on 7 hills, so there will be some small short inclines along the way. We take our time and allow you to take lots of photos, which, I guarantee, you will take lots! We will circle Edinburgh city and will never be more than 2 miles from the centre. I shall give you a background about our route and give historical facts and information.",
+    created_at: "2019-07-05T17:51:34.000Z",
+    updated_at: "2019-09-11T14:20:52.000Z",
+    BookingLink:
+      "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10566525",
+    TourImage: {
+      id: 40,
+      name: "Dean Village.jpg",
+      hash: "6f86a6dfb7d3460b9ac4e2af24a46701",
+      sha256: "Kt1hYQHPfu_c8IBXAkfdq58zxHjCHfEclvZXY7K-3QQ",
+      ext: ".jpg",
+      mime: "image/jpeg",
+      size: "446.01",
+      url: "/uploads/6f86a6dfb7d3460b9ac4e2af24a46701.jpg",
+      provider: "local",
+      public_id: null,
+      created_at: "2019-07-11T11:41:13.000Z",
+      updated_at: "2019-07-11T11:41:13.000Z"
+    }
+  },
     {
-      id: 1,
+      id: 3,
       TourName: "Mary Queen of Scots",
       TourBlurb:
         "Energy rating: Easy, Suitable for families\r\n\r\nDistance: 6 miles\r\n\r\nTerrain: Short distances on roads. Traffic free cycle paths. All paved. Gravel paths within Castle grounds.\r\n\r\nWe shall cycle through the Victorian-era Innocent Tunnel and onto Craigmillar Castle where you can explore where Mary Queen of Scots escaped Edinburgh Castle. (entrance tickets to Craigmillar Castle not included) \r\nIf you have children, we can stop off at an impressive adventure playground and have a snack before cycling to Holyrood Palace to complete the story of Mary. Your tour will finish here.\r\n",
@@ -24,31 +72,55 @@ const tours =[
       }
     },
     {
-      id: 2,
-      TourName: "Edinburgh to Portobello",
+      id: 4,
+      TourName: "Forth Bridges ",
       TourBlurb:
-        "Energy rating: Easy to Moderate. Families welcome if confident on a bike\r\n\r\nDistance: 12 miles\r\n\r\nTerrain: Short distances on roads. Traffic free cycle paths. All paved. \r\n\r\nWe shall start by cycling right beside Arthur's Seat, Edinburgh's extinct volcano and through Duddinston Village, passing Scotland's oldest surviving pub of 1860. We then cycle onto the National Cycle Network 1 path to Portobello beach. There we shall stop for a refreshment outside if sunny or indoors if the Scottish weather is against us! On our return journey we take\r\nthe Victorian-era Innocent Tunnel which was built in 1860 and goes underneath Holyrood Park. ",
-      created_at: "2019-05-19T14:47:30.000Z",
-      updated_at: "2019-09-11T14:19:54.000Z",
+        "Energy rating: Moderate to energetic: suitable for riders with some experience. Families welcome as long as children are confident cyclists. \r\n\r\nDistance: 25 miles\r\n\r\nTerrain: Short distances on roads. Traffic free cycle paths.  Gravel paths within Dalmeny grounds.\r\n\r\nA beautiful traffic free route out to South Queensferry to see the 3 Forth Bridges. This is a stunning ride and if the weather allows, we shall even cycle over the Forth Road Bridge! On our return, we go through the unspoilt Dalmeny House grounds. This tour requires riders to be confident on various terrain, including tarmac, gravel, and uneven road surfaces. \r\n\r\n",
+      created_at: "2019-09-06T19:31:29.000Z",
+      updated_at: "2019-09-11T15:12:35.000Z",
       BookingLink:
-        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10213399",
+        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=11257870",
       TourImage: {
-        id: 70,
-        name: "IMG_3034.jpeg",
-        hash: "39a24130bab04905bb9cef565337823d",
-        sha256: "LUKy_HnzZlyvvvIWvbj5d3Qpflod4RiItU8Qs2TD7vI",
-        ext: ".jpeg",
+        id: 57,
+        name: "8vuRSoJkTVefVEXr82IcxQ.jpg",
+        hash: "5a93d063346d4d36b57361a693795468",
+        sha256: "-LeZn-fm4HQNCU8cwi_hDpPepULfYEow6s1FOyGqG9s",
+        ext: ".jpg",
         mime: "image/jpeg",
-        size: "2945.77",
-        url: "/uploads/39a24130bab04905bb9cef565337823d.jpeg",
+        size: "3187.04",
+        url: "/uploads/5a93d063346d4d36b57361a693795468.jpg",
         provider: "local",
         public_id: null,
-        created_at: "2019-09-11T14:19:54.000Z",
-        updated_at: "2019-09-11T14:19:54.000Z"
+        created_at: "2019-09-06T19:31:29.000Z",
+        updated_at: "2019-09-06T19:31:29.000Z"
       }
     },
     {
-      id: 3,
+      id: 5,
+      TourName: "Entry level mountain biking at Callendar Estate, Falkirk ",
+      TourBlurb:
+        "Perfect for families! This is an easy going mountain bike day out in Falkirk.\r\nRoutes are mainly green and blue, but there are some red routes on site too! \r\nA very well stocked cafe and BMX pump track is next to each other.\r\n\r\nPrice can be negotiated if you have your own transport. \r\nDiscounts for families. \r\n![text](/uploads/3b43c8eed37b407b8abc3512d3b67bed.jpg)\r\n",
+      created_at: "2019-07-11T14:06:40.000Z",
+      updated_at: "2019-08-23T15:49:54.000Z",
+      BookingLink:
+        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10626104",
+      TourImage: {
+        id: 52,
+        name: "trails.jpg",
+        hash: "87a5a36856fd4ac3822dadf535ace5de",
+        sha256: "AudmJG4c_mPxC62pXKYSFgMeaoGQuRf6EuuFcgIQNlg",
+        ext: ".jpg",
+        mime: "image/jpeg",
+        size: "163.81",
+        url: "/uploads/87a5a36856fd4ac3822dadf535ace5de.jpg",
+        provider: "local",
+        public_id: null,
+        created_at: "2019-07-11T14:09:45.000Z",
+        updated_at: "2019-07-11T14:09:45.000Z"
+      }
+    },
+    {
+      id: 6,
       TourName: "Da Vinci Code at Roslin Chapel",
       TourBlurb:
         "Energy rating: Easy to Moderate. This route is perfect for families.\r\n\r\nDistance: 12 miles\r\n\r\nTerrain: This route is ENTIRELY traffic free on the new Sustrans cycle path\r\n\r\nWe shall meet at SHAWFAIR PARK car park and head straight onto the new Sustrans cycle path all the way to Roslin Chapel. \r\nWe will give you time to explore Roslyn Chapel should you wish. (Entrance fee  to Chapel not included)\r\n",
@@ -72,7 +144,7 @@ const tours =[
       }
     },
     {
-      id: 5,
+      id: 7,
       TourName: "Edinburgh to Falkirk Wheel",
       TourBlurb:
         "Energy rating: Moderate to hard \r\n \r\nDistance: 60 miles\r\n\r\nTerrain: Mostly flat. short distance on roads, Paved cycle path, gravel, cobblestone, grit\r\n\r\nThis is a wonderful day out on the bikes if you are looking for a longer ride.\r\nWe shall join the start of the Union Canal which was first in use in 1832 and cycle alongside the canal to Falkirk with scenic rest / refreshment stops en route. You can visit and go on the impressive Falkirk Wheel in action (entrance fee not included) and experience engineering at it’s very best connecting the Forth and Clyde Canal. \r\nThere is a well serviced cafe at The Falkirk Wheel so you can have lunch there or stop 3/4 of way back at The Bridge Inn in Ratho (8 miles outside Edinburgh ) \r\n",
@@ -93,30 +165,6 @@ const tours =[
         public_id: null,
         created_at: "2019-06-03T18:53:23.000Z",
         updated_at: "2019-06-03T18:53:23.000Z"
-      }
-    },
-    {
-      id: 7,
-      TourName: "Family support rides: Tailor made to suit you",
-      TourBlurb:
-        "These sessions will be 2 hours long and I can meet you at your house or an agreed location. I will customise the ride for you depending on your little one's riding ability. These will be fun, family friendly and safe sessions. The cost is £60 per family for 2 hours. \r\n![text](/uploads/1bb345005d244fc7bfa97f1b475dbd16.HEIC)",
-      created_at: "2019-06-07T18:53:48.000Z",
-      updated_at: "2019-08-23T15:47:22.000Z",
-      BookingLink:
-        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10213534",
-      TourImage: {
-        id: 22,
-        name: "163092ee-c9ae-4bc9-b18e-db52b3dd1c34.JPG",
-        hash: "7b6702310afa413d92ede54b3ac8dff8",
-        sha256: "fILdK0ahevnJnyFSOYRSJHBywUkvKrKxTdaGc4GXMCk",
-        ext: ".JPG",
-        mime: "image/jpeg",
-        size: "335.57",
-        url: "/uploads/7b6702310afa413d92ede54b3ac8dff8.JPG",
-        provider: "local",
-        public_id: null,
-        created_at: "2019-06-07T18:53:48.000Z",
-        updated_at: "2019-06-07T18:53:48.000Z"
       }
     },
     {
@@ -145,78 +193,30 @@ const tours =[
     },
     {
       id: 9,
-      TourName: "The ultimate Edinburgh circular tour",
+      TourName: "Family support rides: Tailor made to suit you",
       TourBlurb:
-        "Energy rating: Moderate.  Suitable for people who can ride for 3-4 hours with a stop in between and for families with children of 13+\r\n\r\nDistance: 18 miles\r\n\r\nTerrain:  Some short distances on roads with traffic lights. Traffic free cycle paths which are all paved. Gravel paths. We will be using a cycle ramp purpose built for bikes down a set of steps. \r\n\r\nWe will complete a full circle around Edinburgh city. We start and finish in The Meadows and I will show you the beautiful parts of Edinburgh most visitors do not get the chance to see. Canals, aqueducts, railways, gardens, beaches, mountains and tunnels. This tour is approx 18 miles and we will stop for refreshments on the way. It is mainly flat but Edinburgh is built on 7 hills, so there will be some small short inclines along the way. We take our time and allow you to take lots of photos, which, I guarantee, you will take lots! We will circle Edinburgh city and will never be more than 2 miles from the centre. I shall give you a background about our route and give historical facts and information.",
-      created_at: "2019-07-05T17:51:34.000Z",
-      updated_at: "2019-09-11T14:20:52.000Z",
+        "These sessions will be 2 hours long and I can meet you at your house or an agreed location. I will customise the ride for you depending on your little one's riding ability. These will be fun, family friendly and safe sessions. The cost is £60 per family for 2 hours. \r\n![text](/uploads/1bb345005d244fc7bfa97f1b475dbd16.HEIC)",
+      created_at: "2019-06-07T18:53:48.000Z",
+      updated_at: "2019-08-23T15:47:22.000Z",
       BookingLink:
-        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10566525",
+        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10213534",
       TourImage: {
-        id: 40,
-        name: "Dean Village.jpg",
-        hash: "6f86a6dfb7d3460b9ac4e2af24a46701",
-        sha256: "Kt1hYQHPfu_c8IBXAkfdq58zxHjCHfEclvZXY7K-3QQ",
-        ext: ".jpg",
+        id: 22,
+        name: "163092ee-c9ae-4bc9-b18e-db52b3dd1c34.JPG",
+        hash: "7b6702310afa413d92ede54b3ac8dff8",
+        sha256: "fILdK0ahevnJnyFSOYRSJHBywUkvKrKxTdaGc4GXMCk",
+        ext: ".JPG",
         mime: "image/jpeg",
-        size: "446.01",
-        url: "/uploads/6f86a6dfb7d3460b9ac4e2af24a46701.jpg",
+        size: "335.57",
+        url: "/uploads/7b6702310afa413d92ede54b3ac8dff8.JPG",
         provider: "local",
         public_id: null,
-        created_at: "2019-07-11T11:41:13.000Z",
-        updated_at: "2019-07-11T11:41:13.000Z"
+        created_at: "2019-06-07T18:53:48.000Z",
+        updated_at: "2019-06-07T18:53:48.000Z"
       }
     },
     {
       id: 10,
-      TourName: "Entry level mountain biking at Callendar Estate, Falkirk ",
-      TourBlurb:
-        "Perfect for families! This is an easy going mountain bike day out in Falkirk.\r\nRoutes are mainly green and blue, but there are some red routes on site too! \r\nA very well stocked cafe and BMX pump track is next to each other.\r\n\r\nPrice can be negotiated if you have your own transport. \r\nDiscounts for families. \r\n![text](/uploads/3b43c8eed37b407b8abc3512d3b67bed.jpg)\r\n",
-      created_at: "2019-07-11T14:06:40.000Z",
-      updated_at: "2019-08-23T15:49:54.000Z",
-      BookingLink:
-        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=10626104",
-      TourImage: {
-        id: 52,
-        name: "trails.jpg",
-        hash: "87a5a36856fd4ac3822dadf535ace5de",
-        sha256: "AudmJG4c_mPxC62pXKYSFgMeaoGQuRf6EuuFcgIQNlg",
-        ext: ".jpg",
-        mime: "image/jpeg",
-        size: "163.81",
-        url: "/uploads/87a5a36856fd4ac3822dadf535ace5de.jpg",
-        provider: "local",
-        public_id: null,
-        created_at: "2019-07-11T14:09:45.000Z",
-        updated_at: "2019-07-11T14:09:45.000Z"
-      }
-    },
-    {
-      id: 11,
-      TourName: "Forth Bridges ",
-      TourBlurb:
-        "Energy rating: Moderate to energetic: suitable for riders with some experience. Families welcome as long as children are confident cyclists. \r\n\r\nDistance: 25 miles\r\n\r\nTerrain: Short distances on roads. Traffic free cycle paths.  Gravel paths within Dalmeny grounds.\r\n\r\nA beautiful traffic free route out to South Queensferry to see the 3 Forth Bridges. This is a stunning ride and if the weather allows, we shall even cycle over the Forth Road Bridge! On our return, we go through the unspoilt Dalmeny House grounds. This tour requires riders to be confident on various terrain, including tarmac, gravel, and uneven road surfaces. \r\n\r\n",
-      created_at: "2019-09-06T19:31:29.000Z",
-      updated_at: "2019-09-11T15:12:35.000Z",
-      BookingLink:
-        "https://app.acuityscheduling.com/schedule.php?owner=17728702&appointmentType=11257870",
-      TourImage: {
-        id: 57,
-        name: "8vuRSoJkTVefVEXr82IcxQ.jpg",
-        hash: "5a93d063346d4d36b57361a693795468",
-        sha256: "-LeZn-fm4HQNCU8cwi_hDpPepULfYEow6s1FOyGqG9s",
-        ext: ".jpg",
-        mime: "image/jpeg",
-        size: "3187.04",
-        url: "/uploads/5a93d063346d4d36b57361a693795468.jpg",
-        provider: "local",
-        public_id: null,
-        created_at: "2019-09-06T19:31:29.000Z",
-        updated_at: "2019-09-06T19:31:29.000Z"
-      }
-    },
-    {
-      id: 12,
       TourName: "Bike Service to get you back on the road",
       TourBlurb: "\r\n![text](/uploads/80c6a6e4f0b14dd6a35376235638e9f5.jpg)",
       created_at: "2019-09-11T12:34:00.000Z",
